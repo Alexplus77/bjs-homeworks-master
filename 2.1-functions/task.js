86e995e809b8fb2a77dad7977569ed69b03ea0be
Alexplus77 <alexplus77@mail.ru>
2021-05-25 10:39:16 +0200

    ww

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -40,8 +40,8 @@ function showSolutionsMessage(a, b, c) {
 
 showSolutionsMessage(1, 2, 3);
 
-//! Задача №2 вариант 1
-const dataN = {
+//! Задача №2 
+const data = {
   algebra: [2, 5, 3, 4, 3],
   geometry: [4, 5, 3, 3, 2, 5, 5],
   russan: [3, 3, 5, 4],
@@ -53,87 +53,6 @@ const dataN = {
   franch: [4, 5, 2, 3],
   geografy: [4, 5, 4, 3],
 };
-
-function getAverageScore(data2) {
-  //return obj[data2]
-}
-
-function getAverageMark(data2) {
-  const arr4 = getAverageScore(data2);
-  //console.log(arr4);
-}
-
-getAverageScore({
-  algebra: [2, 5, 3, 4, 3],
-  geometry: [4, 5, 3, 3, 2, 5, 5],
-  russan: [3, 3, 5, 4],
-  physycks: [5, 4, 3, 5, 2],
-  music: [3, 4, 5, 3],
-  englisn: [4, 5, 3, 4, 4],
-  poetry: [5, 4, 4, 4, 3, 3, 5, 5],
-  chemystry: [3, 4, 2],
-  franch: [4, 5, 2, 3],
-  geografy: [4, 5, 4, 3],
-});
-
-//! вариант 2
-
-function getAverageScore2(data) {
-  const newArr = {};
-  for (let [key, value] of Object.entries(data)) {
-    const arr = [key, [value.reduce((acc, elem) => acc + elem / value.length)]];
-
-    const arr2 = value.reduce((acc, elem) => acc + elem / value.length);
-  }
-  //console.log(newArr)
-}
-
-function getAverageMark2(data) {
-  const arr = getAverageScore2(data);
-}
-
-getAverageScore2({
-  algebra: [2, 5, 3, 4, 3],
-  geometry: [4, 5, 3, 3, 2, 5, 5],
-  russan: [3, 3, 5, 4],
-  physycks: [5, 4, 3, 5, 2],
-  music: [3, 4, 5, 3],
-  englisn: [4, 5, 3, 4, 4],
-  poetry: [5, 4, 4, 4, 3, 3, 5, 5],
-  chemystry: [3, 4, 2],
-  franch: [4, 5, 2, 3],
-  geografy: [4, 5, 4, 3],
-});
-
-const array = {
-  algebra: [2, 5, 3, 4, 3],
-  geometry: [4, 5, 3, 3, 2, 5, 5],
-  russan: [3, 3, 5, 4],
-  physycks: [5, 4, 3, 5, 2],
-  music: [3, 4, 5, 3],
-  englisn: [4, 5, 3, 4, 4],
-  poetry: [5, 4, 4, 4, 3, 3, 5, 5],
-  chemystry: [3, 4, 2],
-  franch: [4, 5, 2, 3],
-  geografy: [4, 5, 4, 3],
-};
-
-const newArray = [];
-const obj = { newArray };
-
-for (let i in array) {
-  newArray[i] = array[i].reduce((acc, elem) => acc + elem) / array[i].length;
-}
-newArray.forEach((o, i) => {
-  obj[o] = o;
-});
-
-console.log(obj);
-
-//! Новый
-
-
-
 
 
 const grades = [5, 5, 3, 4, 3, 5]; // 4.166666666666667
@@ -155,4 +74,4 @@ function getAverageScore(data) {
 
   return result;
 }
-getAverageScore(data);
+console.log(getAverageScore(data));
