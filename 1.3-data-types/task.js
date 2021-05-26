9574b2a2e92dabb54ea8d4402d5985b7a83bb3e1
Alexplus77 <alexplus77@mail.ru>
2021-05-26 15:18:22 +0200

    сделал 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,49 @@
+//! Задача №1
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  "use strict";
+
+  const percentNum = +percent;
+  const contributionNum = Number(contribution);
+  const amountNum = Number(amount);
+
+  const P = percentNum / 100 / 12;
+  let S = amountNum - contributionNum;
+  let dateNow = new Date();
+  let n =
+    date.getMonth() -
+    dateNow.getMonth() +
+    12 * (date.getFullYear() - dateNow.getFullYear());
+
+  let resultYear = S * (P + P / ((1 + P) ** n - 1)) * 12;
+
+  let totalAmount = Math.floor(resultYear * 100) / 100;
+
+  if (isNaN(percentNum)) {
+    return `Параметр percent содержит неправильное значение ${percent} `;
+  } else if (isNaN(contributionNum)) {
+    return `Параметр contribution содержит неправильное значение ${contribution} `;
+  } else if (isNaN(amountNum)) {
+    return `Параметр amount содержит неправильное значение ${amount} `;
+  } else {
+    return totalAmount;
+  }
+
+  // код для задачи №1 писать здесь
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+if (name.trim() !== ''){
+    let nameNew = name
+    return `Привет, мир! Меня зовут ${nameNew}`
+} else {
+    let nameNew = 'Аноним' 
+    return `Привет, мир! Меня зовут ${nameNew}`
+}
+    
+    
+    
+  // код для задачи №2 писать здесь
+   //
 }
