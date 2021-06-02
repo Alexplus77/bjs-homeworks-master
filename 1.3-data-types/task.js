ccecc603e46770cb9e93a4e60e9759d27f52f79c
Alexplus77 <alexplus77@mail.ru>
2021-06-02 19:13:54 +0200

    jj

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -14,7 +14,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   const percentNum = Number(percent);
   const contributionNum = Number(contribution);
   const amountNum = Number(amount);
-
+const arr ={percentNum: ""}
   const P = percentNum / 100 / 12;
   let S = amountNum - contributionNum;
   let n = parsDate(date);
@@ -22,11 +22,12 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let resultYear = S * (P + P / ((1 + P) ** n - 1)) * n;
 
   let totalAmount = Math.round(resultYear * 100) / 100;
-  
-function validateParam (param){isNaN(param)|| param <0 }
+
+  function validateParam (param){ return isNaN(param) || param <0 } 
+
 
   for ( const param of arguments){
-    
+   
 if (validateParam(param)){
   return `Параметр ${param} содержит неправильное значение ${param}`
 } else { return totalAmount}
