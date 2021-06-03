ede065bcfe569f2b58dba108a88f56470bc6e120
Alexplus77 <alexplus77@mail.ru>
2021-06-03 20:01:11 +0200

    формат

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,20 +1,27 @@
 //!Задача№1
-String.prototype.isPalindrome= function(){
-   let value = this.toLowerCase().replace(/\s+/g, '')
-    
-    let value2 = value.toLowerCase().replace(/\s+/g, '').split('').reverse().join('')
+String.prototype.isPalindrome = function () {
+  let value = this.toLowerCase().replace(/\s+/g, "");
 
-if(value2 === value){return true}else{return false}
-}
+  let value2 = value
+    .toLowerCase()
+    .replace(/\s+/g, "")
+    .split("")
+    .reverse()
+    .join("");
 
-
+  if (value2 === value) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  // код для задачи №2 писать здесь
+  // return averageMark
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  // код для задачи №3 писать здесь
+  // return verdict
 }
