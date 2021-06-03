e48c25ced2d2883e6cca7a1d736252a7837af167
Alexplus77 <alexplus77@mail.ru>
2021-06-03 19:04:48 +0200

    2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,15 +1,16 @@
 String.prototype.isPalindrome
-function isPalindrome(value) {
+
+const isPalindrome=(value)=>{
      this.value = value.toLowerCase().replace(/\s+/g, '')
-    console.log(this.value)
+    console.log(value)
 this.value2 = value.toLowerCase().replace(/\s+/g, '').split('').reverse().join('')
 
 if(this.value2 === this.value){return true}else{return false}
 
 }
+ 
+console.log(isPalindrome('А роза упала на лапу Азора'))
 
-//console.log(isPalindrome('А роза упала на лапу Азора'))
-//console.log(isPalindrome('А роза упала на лапу Азора'))
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
