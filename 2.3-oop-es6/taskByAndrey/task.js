46fac10fb8056c1545d8353d1a389267959fd3f9
Alexplus77 <alexplus77@mail.ru>
2021-06-15 16:04:54 +0200

    jjj

--- 2.3-oop-es6/taskByAndrey/task.js
+++ 2.3-oop-es6/taskByAndrey/task.js
@@ -1,27 +1,21 @@
 
 const str='abbcccddddeeeeefffffffffffggggggggggghhhhhhhhhhijkabcdfabcdfalexeyisaprogrammer'
+
+const letters= str.split('')
 const obj = {}
-let count = 1
 
+function objectAdd(){
 
-function objectAdd(str){
-const leters= str.split('')
-
-for(const leter of leters){
-    
-   obj[leter]=count
-   //console.log(leter)
-   if(Object.keys(obj).includes(leter)){
-    obj[leter]=count+1
-console.log(obj)
-   }
-
-
-
+for(const elem of letters){
+   const count = 1
+    if(elem){
+       obj[elem]=count+1   
+               
+    }    
 }
  
-
+console.log(obj)
 
 }
 
-objectAdd(str)
+objectAdd()
