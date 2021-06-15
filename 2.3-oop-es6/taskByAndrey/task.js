ae838aac01839c8642aa53f096f4c008aade55c4
Alexplus77 <alexplus77@mail.ru>
2021-06-15 13:39:35 +0200

    kkk

--- 2.3-oop-es6/taskByAndrey/task.js
+++ 2.3-oop-es6/taskByAndrey/task.js
@@ -2,18 +2,24 @@
 const str='abbcccddddeeeeefffffffffffggggggggggghhhhhhhhhhijkabcdfabcdfalexeyisaprogrammer'
 const obj = {}
 let count = 1
+
+
 function objectAdd(str){
 const leters= str.split('')
 
 for(const leter of leters){
-    obj[leter]=leter  
- if(obj[leter]===leter){  
-    obj[leter]=count++
+    
+   obj[leter]=count
+   //console.log(leter)
+   if(Object.keys(obj).includes(leter)){
+    obj[leter]=count+1
+console.log(obj)
+   }
+
+
 
 }
-   
-}
- console.log(obj)
+ 
 
 
 }
