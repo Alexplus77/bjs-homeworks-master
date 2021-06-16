dedd2ac38ee779b5a937b660fef2cf9a3460606d
Alexplus77 <alexplus77@mail.ru>
2021-06-16 07:23:59 +0200

    Сделал задачу 2

--- 2.3-oop-es6/taskByAndrey/task.js
+++ 2.3-oop-es6/taskByAndrey/task.js
@@ -16,3 +16,76 @@ function objectAdd() {
 }
 
 objectAdd();
+//! Задание №2
+console.log("Задание №2");
+
+const data = [
+  {
+    country: "Россия",
+
+    city: "Москва",
+  },
+
+  {
+    country: "Беларусь",
+
+    city: "Минск",
+  },
+
+  {
+    country: "Россия",
+
+    city: "Питер",
+  },
+
+  {
+    country: "Россия",
+
+    city: "Владивосток",
+  },
+
+  {
+    country: "Украина",
+
+    city: "Львов",
+  },
+
+  {
+    country: "Беларусь",
+
+    city: "Могилев",
+  },
+
+  {
+    country: "Украина",
+
+    city: "Киев",
+  },
+
+  {
+    country: "Россия",
+
+    city: "Владикавказ",
+  },
+
+  {
+    country: "Беларусь",
+
+    city: "Брест",
+  },
+
+  {
+    country: "Россия",
+
+    city: "Калининград",
+  },
+];
+
+const townOfContry = {};
+for (const obj of data) {
+  const country = obj.country;
+  const city = obj.city;
+  townOfContry[country] = townOfContry[country] || [];
+  townOfContry[country].push(city);
+  console.log(townOfContry);
+}
