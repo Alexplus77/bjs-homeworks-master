f17c1aa4a72ad0fe4753164a09badd9938ae3529
Alexplus77 <alexplus77@mail.ru>
2021-06-23 16:45:34 +0200

    ddd

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,8 +1,7 @@
 //! Задача №1
 function parseCount(value) {
   if (!Number.parseInt(value)) {
-    const parsError = new Error("Невалидное значение");
-    throw parsError;
+    throw new Error("Невалидное значение");
   }
   return Number.parseInt(value);
 }
@@ -11,6 +10,51 @@ function validateCount(value) {
   try {
     return parseCount(value);
   } catch (error) {
-    return error
+    return error;
+  }
+}
+
+//! Задача №2
+
+class Triangle {
+  constructor(a, b, c) {
+    if (this.validate(a, b, c)) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  validate(a, b, c) {
+    if (a + b < c || a + c < b || b + c < a) {
+      return true;
+    }
+  }
+
+  getPerimeter() {
+    const perimetr = this.a + this.b + this.c;
+    if (this.validate()) {
+      throw new Error("Ошибка! Треугольник не существует");
+    }
+    return perimetr;
+  }
+
+  getArea() {
+    const p = this.getPerimeter() / 2;
+    const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    if (this.validate()) {
+      throw new Error("Ошибка! Треугольник не существует");
+    }
+    return Math.round(s * 1000) / 1000;
+  }
+}
+function getTriangle(a, b, c) {
+  const triangle = new Triangle(a, b, c);
+  try {
+    return triangle;
+  } catch (error) {
+    triangle.getPerimeter();
+    triangle.getArea();
+    return error;
   }
 }
