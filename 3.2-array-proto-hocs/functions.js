dc6e4d58d543dd76bbbaa5566546f133fec6577f
Alexplus77 <alexplus77@mail.ru>
2021-06-23 22:53:18 +0200

    зазача 1 раздел 3.2 сделана

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -1,3 +1,4 @@
+//! Задача №1
 console.clear();
 const weapons = [
   new Knife(),
@@ -23,9 +24,11 @@ function hasReliableWeapons(durability) {
 }
 
 function getReliableWeaponsNames(durability) {
-    return weapons.filter((elem) => elem.durability > durability).map(elem=> elem.name)    
+  return weapons
+    .filter((elem) => elem.durability > durability)
+    .map((elem) => elem.name);
 }
 
-
-
-
+function getTotalDamage() {
+  return weapons.map((elem) => elem.attack).reduce((elem, acc) => elem + acc);
+}
