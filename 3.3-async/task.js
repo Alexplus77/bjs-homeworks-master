743a8fe4522b4566ce974d2d2b62cec5792c3304
Alexplus77 <alexplus77@mail.ru>
2021-07-11 20:53:41 +0200

    ggg

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -1,66 +1,57 @@
 class AlarmClock {
-    constructor() {
-        this.alarmCollection = [],
-            this.timerId
-    }
-    addClock(time, callback, id) {
-        
-        if (!id) {
-            throw new Error("ID не задан");
-        }
-        if (this.alarmCollection.find((n) => n.id === id)) {
-            console.error("Звонок с таким ID уже существует");
-            return;
-        } else {
-        
-            this.alarmCollection.push({
-                id,
-                time,
-                callback,
-            });
-            console.log(this.alarmCollection);
-        }
-    }
-    removeClock(id) {
-        if (
-            this.alarmCollection.find((elem, i) => {
-                if (elem.id === id) {
-                    this.alarmCollection.splice(i, 1);
-                    return true;
-                }
-            })
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    getCurrentFormattedTime() {
-        const timeNow = new Date().toLocaleTimeString().slice(0, -3)
-        return timeNow
-    }
-    checkClock() {
-       return this.alarmCollection.find((elem) => {
-            if (elem.time === this.getCurrentFormattedTime()) {
-              return elem.time
-          }
-        });
-        
-    }
-
-    start() {    
-        
-        if (this.checkClock()) {
-            console.log(this.getCurrentFormattedTime());
-            return this.alarmCollection.find((elem) => {
-                
-                elem.callback
-            })
-        }
+  constructor() {
+    (this.alarmCollection = []), this.timerId;
   }
-    stop() {
-      
+  addClock(time, callback, id) {
+    if (!id) {
+      throw new Error("ID не задан");
+    }
+    if (this.alarmCollection.find((n) => n.id === id)) {
+      console.error("Звонок с таким ID уже существует");
+      return;
+    } else {
+      this.alarmCollection.push({
+        id,
+        time,
+        callback,
+      });
+      console.log(this.alarmCollection);
+    }
   }
+  removeClock(id) {
+    if (
+      this.alarmCollection.find((elem, i) => {
+        if (elem.id === id) {
+          this.alarmCollection.splice(i, 1);
+          return true;
+        }
+      })
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  getCurrentFormattedTime() {
+    const timeNow = new Date().toLocaleTimeString().slice(0, -3);
+    return timeNow;
+  }
+  checkClock() {
+    return this.alarmCollection.find((elem) => {
+      if (elem.time === this.getCurrentFormattedTime()) {
+        return true;
+      }
+    });
+  }
+  start() {
+    if (this.checkClock()) {
+      console.log(this.getCurrentFormattedTime());
+      return this.alarmCollection.find((elem) => {
+        elem.callback;
+      });
+    }
+  }
+  stop() {}
   printAlarms() {}
   clearAlarms() {}
 }
