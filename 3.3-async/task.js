0639d2735c2ecb86979eef0a010d000243d98c87
Alexplus77 <alexplus77@mail.ru>
2021-07-11 20:51:51 +0200

    hh

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -1,44 +1,66 @@
 class AlarmClock {
-  constructor() {
-    this.alarmCollection = [];
-  }
-  addClock(time, callback, id) {
-    if (!id) {
-      throw new Error("ID не задан");
+    constructor() {
+        this.alarmCollection = [],
+            this.timerId
     }
-    if (this.alarmCollection.find((n) => n.id === id)) {
-      console.error("Звонок с таким ID уже существует");
-      return;
-    } else {
-      this.alarmCollection.push({
-        id,
-        time,
-        callback,
-      });
-      console.log(this.alarmCollection);
-    }
-  }
-  removeClock(id) {
-    if (
-      this.alarmCollection.find((elem, i) => {
-        if (elem.id === id) {
-          this.alarmCollection.splice(i, 1);
-          return true;
+    addClock(time, callback, id) {
+        
+        if (!id) {
+            throw new Error("ID не задан");
         }
-      })
-    ) {
-      return true;
-    } else {
-      return false;
+        if (this.alarmCollection.find((n) => n.id === id)) {
+            console.error("Звонок с таким ID уже существует");
+            return;
+        } else {
+        
+            this.alarmCollection.push({
+                id,
+                time,
+                callback,
+            });
+            console.log(this.alarmCollection);
+        }
     }
+    removeClock(id) {
+        if (
+            this.alarmCollection.find((elem, i) => {
+                if (elem.id === id) {
+                    this.alarmCollection.splice(i, 1);
+                    return true;
+                }
+            })
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    getCurrentFormattedTime() {
+        const timeNow = new Date().toLocaleTimeString().slice(0, -3)
+        return timeNow
+    }
+    checkClock() {
+       return this.alarmCollection.find((elem) => {
+            if (elem.time === this.getCurrentFormattedTime()) {
+              return elem.time
+          }
+        });
+        
+    }
+
+    start() {    
+        
+        if (this.checkClock()) {
+            console.log(this.getCurrentFormattedTime());
+            return this.alarmCollection.find((elem) => {
+                
+                elem.callback
+            })
+        }
   }
-  getCurrentFormattedTime() {
-    const hh = new Date().getHours();
-    const mm = new Date().getMinutes();
-    return [hh, mm].join(":");
+    stop() {
+      
   }
-  start() {}
-  stop() {}
   printAlarms() {}
   clearAlarms() {}
 }
@@ -52,11 +74,12 @@ console.log(
   phoneAlarm.addClock("10:35", () => console.log("Пора вставать"), 2)
 );
 console.log(
-  phoneAlarm.addClock("11:35", () => console.log("Пора вставать"), 3)
+  phoneAlarm.addClock("11:35", () => console.log("Пора вставать"), 2)
 );
 console.log(
-  phoneAlarm.addClock("12:35", () => console.log("Пора вставать"), 4)
+  phoneAlarm.addClock("20:50", () => console.log("Пора вставать"), 3)
 );
-console.log(phoneAlarm.removeClock(3));
+//console.log(phoneAlarm.removeClock(3));
 
 console.log(phoneAlarm.getCurrentFormattedTime());
+console.log(phoneAlarm.checkClock());
